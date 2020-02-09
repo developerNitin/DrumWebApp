e349bf40da94b68856727e3d924bd6f5fcb9cb4e
Nitin Birdi <nitin.birdi@icloud.com>
2020-02-10 01:31:27 +0530

    version 1.2

--- index.js
+++ index.js
@@ -1,9 +1,39 @@
-
 var numberOfDrumButton = document.querySelectorAll(".drum").length;
 
 for (var i = 0; i < numberOfDrumButton; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-    alert("I got clicked!");
-
-  })
+    var buttonInnerHtml = this.innerHTML;
+       switch (buttonInnerHtml) {
+      case "w":
+        var tom1 = new Audio("sounds/tom-1.mp3");
+        tom1.play();
+        break;
+      case "a":
+        var tom2 = new Audio("sounds/tom-2.mp3");
+        tom2.play();
+        break;
+      case "s":
+        var tom3 = new Audio("sounds/tom-3.mp3");
+        tom3.play();
+        break;
+      case "d":
+        var tom4 = new Audio("sounds/tom-4.mp3");
+        tom4.play();
+        break;
+      case "j":
+        var kick = new Audio("sounds/kick-bass.mp3");
+        kick.play();
+        break;
+      case "k":
+        var snare = new Audio("sounds/snare.mp3");
+        snare.play();
+        break;
+      case "l":
+        var crash = new Audio("sounds/crash.mp3");
+        crash.play();
+        break;
+      default:
+        console.log(buttonInnerHtml);
+    }
+  });
 }
